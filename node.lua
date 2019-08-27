74ad19177e457ea74d70c5ab5b901cb0de247dc9
Erte148 <rt.pskov@gmail.com>
2019-08-27 07:11:56 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,6 +2,8 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font "font.ttf"
 local count = 0
+local num=0
+
 
 util.data_mapper{
     counter = function(counter)
@@ -10,8 +12,10 @@ util.data_mapper{
 }
 
 function node.render()
+    if count==17 then  num=555 end
+    if count==18 then  num=666 end    
     gl.clear(0,0,0,1)
     font:write(30, 10, "Motion Detected", 100, .5,.5,.5,1)
-    countStr = tostring(count)
+    countStr = tostring(num)
     font:write(250, 300, countStr, 64, 1,1,1,1)
 end
