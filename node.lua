e96834ae7abdb6d965bbe83208dfd8828bde37bc
Erte148 <rt.pskov@gmail.com>
2019-08-27 07:22:55 +0300

    Update node.lua

--- node.lua
+++ node.lua
@@ -12,10 +12,12 @@ util.data_mapper{
 }
 
 function node.render()
-   
+   if count==17 then  num=555 
+    elseif count==18 then  num=666 
+        end
      
     gl.clear(0,0,0,1)
     font:write(30, 10, "Motion Detected", 100, .5,.5,.5,1)
-    countStr = tostring(count)
+    countStr = tostring(num)
     font:write(250, 300, countStr, 64, 1,1,1,1)
 end
